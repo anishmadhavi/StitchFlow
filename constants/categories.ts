f921e5287dac11c7087c43f83df3e72024a397d5
Tech Softwares <anishmadhavi@gmail.com>
2026-01-02 10:17:37 +0530

    Create categories.ts

---
+++ constants/categories.ts
@@ -0,0 +1,5 @@
+export const CATEGORY_SIZES = {
+  "Maternity Wear": ["36 - S", "38 - M", "40 - L", "42 - XL", "44 - XXL", "46 - 3XL"],
+  "Kidswear": ["22", "24", "26", "28", "30", "32", "34"],
+  "Combo Sizes": ["36 - 38 ( S - M )", "40 - 42 ( L - XL )", "44 - 46 ( XXL - 3XL)"]
+};
